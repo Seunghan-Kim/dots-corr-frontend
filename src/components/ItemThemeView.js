0ea17d2d25786e28efb47945bafc232bbce4fdba
Seunghan-Kim <stop@006@gmail.com>
2021-06-26 14:24:05 +0900

    Refactoring variables, useState ...

--- src/components/ItemThemeView.js
+++ src/components/ItemThemeView.js
@@ -19,103 +19,71 @@ let refData = String(parseInt(today) - 1);
 
 console.log(refData)
 
-function getWindowDimensions() {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-      width,
-      height
-    };
-  }
-
 const ItemThemeView = () => {
 
 
     // const backendUrl = 'http://localhost:8080/';
     const backendUrl = 'https://backend-v31-s5icpxfxda-uc.a.run.app/'
 
-
-    const [topItems, setTopItems] = useState([])
-    const [topThemes, setTopThemes ] = useState([])
-
-    const [selectedItem, setSelectedItem ] = useState('')
-    const [selectedItemName, setSelectedItemName] = useState('')
-    const [selectedTheme, setSelectedTheme] = useState('')
+    const [top30ListData, setTop30ListData] = useState([]);
+    const [selectedTop30, setSelectedTop30] = useState({})
 
     const [itemChartData, setItemChartData] = useState([])
-    const [themeChartData, setThemeChartData] = useState([])
-    
-    const [placeholderText, setPlaceholderText] = useState('')
-    const [whoseTurn, setWhoseTurn] = useState('item')
-    const [itemIndex, setItemIndex] = useState(0)
-    const [themeIndex, setThemeIndex] = useState(0)
-    const [dateRef1, setDateRef] = useState(refData)   
+    const [dateRef1, setDateRef] = useState(refData)
 
     const [corrData, setCorrData] = useState([])
     const isFirstRender = useRef(true)
 
-    const heightRef = useRef(getWindowDimensions().height)
+    useEffect(() => { //시작할때 데이터 불러오기 + 종목, 테마 데이터 불러오기
+        fetch(`${backendUrl}topitems/${dateRef1}`)
+        .then(response => response.json())
+        .then(json => setTop30ListData(json.data))        
+        } , [dateRef1]) 
+        
+    useEffect(() => { //종목 데이터 다 들어오면 첫번째 종목코드 선정하고 차트를 위한 데이터 불러오기
+
+        if (top30ListData.length !== 0 && isFirstRender.current){
+            let code = top30ListData[0].code
+            let name = top30ListData[0].name
+            // setSelectedTop30({'code':code, 'name':name})
+            getChartData(code, 'top30', name)
+            isFirstRender.current = false
+        }
+    }, [top30ListData])
 
     const getChartData = (code, kind, name) => {
-        console.log(this)
-        let url = backendUrl + 'chartdata_' + kind
-        console.log(code, name)
-
+        let url = `${backendUrl}chartdata_${kind}`
         let newCode = {'code' : code, 'date': dateRef1 }
-
         fetch(url, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(newCode)
         }).then(response => response.json())
-          .then(json => {
-              console.log('json', json)
-              if (kind === 'top30') {
+          .then(json => {             
                   setItemChartData(json.data);
-                  console.log(json.data)
-                  setSelectedItem(code)
-                  setSelectedItemName(name)
-                                
-            }
-              else if (kind === 'theme') {
-                  setThemeChartData(json.data);
-                  setSelectedTheme(code)                
-                }
+                  console.log('price data called',json.data)
+                //   setSelectedItem(code)
+                //   setSelectedItemName(name)
+                //   setSelectedItemOfTop30({'code':code, 'name':name})
+                  setSelectedTop30({'code':code, 'name':name})
           }).then(()=>{
               getCorrData(code)
             })        
     }
 
-    const getCorrData = (code) => {
-        console.log(code)
-        fetch(backendUrl + `corr2/${code}`)
+    const getCorrData = (code, sortOrder) => {
+        let sortType = sortOrder
+        if (!sortType){
+            sortType = ''
+        } 
+        let fetchUrl = `${backendUrl}corr2/${code}/${sortOrder}`
+        fetch(fetchUrl)
         .then(response => response.json())
-        .then(json => {
-            
+        .then(json => {            
             setCorrData(json.data)
             console.log('corr', json.data)
         })
     }
-
-    
-    useEffect(() => { //시작할때 데이터 불러오기 + 종목, 테마 데이터 불러오기
-        fetch(backendUrl + `topitems/${dateRef1}`)
-        .then(response => response.json())
-        .then(json => setTopItems(json.data))        
-        } , [dateRef1]) 
-
-        
-    useEffect(() => { //종목 데이터 다 들어오면 첫번째 종목코드 선정하고 차트를 위한 데이터 불러오기
-        if (topItems.length !== 0 && isFirstRender.current){
-            let selectedItemCode = topItems[0].code
-            let selectedItemName = topItems[0].name
-            console.log('firts code',selectedItemCode)
-            setSelectedItem(selectedItemCode)
-            setSelectedItemName(selectedItemName)
-            // getItemChartData(selectedCode, 'top30')
-            getChartData(selectedItemCode, 'top30', selectedItemName)
-            isFirstRender.current = false
-        }
-    }, [topItems])
 
     const decreaseDate = () => {
         isFirstRender.current = true
@@ -133,17 +101,14 @@ const ItemThemeView = () => {
         console.log('fwd')
     }
     
+    const resortCorrCard = (sortOrder) => {
+        getCorrData(selectedTop30.code, sortOrder);
+    }
+
     return(
         <div className='mainContainer'>
-            {/* <MainSearch placeholderText={placeholderText}/> */}
-            
-            {/* <HorizontalScroll kind={"theme"} data={topThemes} clickHandler={getChartData}/> */}
-            {/* <IndexChart data={itemChartData} code={selectedItem}/> */}
-            {/* <IndexChart data={themeChartData} code={selectedTheme}/> */}
-            {/* <div style={{backgroundColor:'white'}}>{count}{text}</div> */}
-            {/* <ResponsiveContainer width="100%" height="100%"> */}
             <div className='chartContainer'>
-                <div className='chartItemName'>{selectedItemName}</div>
+                <div className='chartItemName'>{selectedTop30.name}</div>
                 <LineChart
                     width={360}
                     height={250}
@@ -164,24 +129,27 @@ const ItemThemeView = () => {
                     {/* <Line type="monotone" dataKey="y" stroke="#8884d8" activeDot={{ r: 8 }} /> */}
                     <Line type="monotone" dataKey="종가" stroke="#82ca9d" dot={false}/>
                 </LineChart>
-                               
-                
-                
             </div>
+
             <div className='itemScrollBoxContainer'>           
                 <div className='top30HeraderContainer'>
-                    <div className='top30DateText'>{selectedItemName}</div>
+                    <div className='top30DateText'>{selectedTop30.name}</div>
                     <div className='top30HeaderText'>와 주가 변동이 비슷한 종목들</div>
                 </div>
                 <HorizontalScrollCorr data={corrData} clickHandler={getChartData}/>   
             </div>
-            
+
+            <div className='resortCorrCardBtnContainer'>
+                <Button variant="contained" color="primary" onClick={()=>resortCorrCard('n_days')}>n_days</Button>
+                {/* <Button variant="contained" color="primary" onClick={()=>resortCorrCard('')}>Re-sort</Button> */}
+            </div>
+
             <div className='itemScrollBoxContainer'>
                 <div className='top30HeraderContainer'>
                     <div className='top30DateText'>{dateRef1.slice(4,8)}</div>
                     <div className='top30HeaderText'>상승률 TOP30</div>
                 </div>
-                <HorizontalScroll kind={"top30"} data={topItems} clickHandler={getChartData}/>
+                <HorizontalScroll kind={"top30"} data={top30ListData} clickHandler={getChartData}/>
             </div>
 
             <div className='dateSelectorContainer'>
@@ -189,11 +157,8 @@ const ItemThemeView = () => {
                 <div className='dateText'>{dateRef1}</div>
                 <Button variant="contained" color="primary" onClick={()=>increaseDate()}>앞으로</Button>
             </div>
-            
-            
         </div>
     )
-    
 }
 
 export default ItemThemeView;
